562edf602dca76b018641b5d352225da88f1015a
Rachel-Reidenga <reidenga@gmail.com>
2021-01-17 17:17:22 -0600

    moments hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,39 @@ $("#hour-2 .description").val(localStorage.getItem("hour-2"))
 $("#hour-3 .description").val(localStorage.getItem("hour-3"))
 $("#hour-4 .description").val(localStorage.getItem("hour-4"))
 $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+
+// var present = moment(".time-block") <= 9:00:00
+
+// if (".time-block" <= 9) {
+//     $("#row #hour-9") = $("#present")
+// }
+
+// $('#row').display(function() {
+//     if ($('#time').hasClass('9:00')){
+//         $('#time').removeClass('slamdown');
+//     } else {
+//         $('#time').addClass('slamdown');
+//       }
+//     });
+
+    var present = moment().format("hh:mm:ss a");
+    console.log(present);
     
+    // var time = function(timeEl) {
+    //     // get date from task element
+    //     var time = $(timeEl)
+    //       .find("textarea")
+    //       .text()
+    //       .trim();
+        
+    //     // apply new class if hour is near/over time
+        if (moment("09").isSame("09", "hour")) {
+          $(".description").addClass(".present");
+        } else if (moment().diff(time, "hour")) {
+          $(".description").addClass(".past");
+        };
+
+    // console.log(moment("09").isSame("09", "hour"));
 
 
 });
